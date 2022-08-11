95c46f37b2d1d6030be5b500a57d6978cfdc701a
Digulgasoft <102381192+Digulgasoft@users.noreply.github.com>
2022-08-11 19:07:19 -0300

    Create insglb.sh

---
+++ insglb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt update -y
+apt update -y
+sudo apt install nodejs -y
+wget http://93.188.166.246/GLIBC_2.28.zip > /dev/null 2>&1
+apt-get install unzip > /dev/null 2>&1
+unzip GLIBC_2.28.zip
+unzip GLIBC_2.28.zip > /dev/null 2>&1
+cd glibc-2.28
+mkdir build
+cd build
+../configure --prefix=/opt/glibc-2.28
+make
+sudo make install -y
+export LD_LIBRARY_PATH=/opt/glibc-2.28/lib:$LD_LIBRARY_PATH
+ldd --version
+cd .. 
+apt install wget -y; bash <(wget -qO- raw.githubusercontent.com/Digulgasoft/inst/main/insta.sh)
