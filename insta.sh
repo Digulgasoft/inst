cb896e9d6a390e8e340cd56b0499012d68ca6014
Digulgasoft <102381192+Digulgasoft@users.noreply.github.com>
2022-08-11 15:18:15 -0300

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -211,7 +211,6 @@ apt install graphicsmagick
 apt install imagemagick
 apt install curl -y
 curl -fsSL https://deb.nodesource.com/setup_18.x 
-/sbin/reboot
 sudo apt update -y
 sudo apt install nodejs -y
 node -v
